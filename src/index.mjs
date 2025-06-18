1aa505b58a60cc6582e403869d8791b25b06d355
Libro <libro.1115@gmail.com>
2025-06-18 15:06:40 +0900

    ToDo入力

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,31 @@
-import "./style.css"
+
+const onClickAdd = ()=>{
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value=""
+    //li生成
+    const li = document.createElement("li");
+    //div生成
+    const div = document.createElement("div");
+    div.className="list-row";
+    //p生成
+    const p = document.createElement("p");
+    p.className="todo-item";
+    p.innerText=inputText;
+    //完了ボタン生成
+    const completeButton = document.createElement("button")
+    completeButton.innerText = "完了"
+    //削除ボタン生成
+    const deleteButton = document.createElement("button")
+    deleteButton.innerText = "削除"
+    //追加
+    div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+    li.appendChild(div);
+    document.getElementById("incomplete-list").appendChild(li);
+    console.log(li);
+}
+
+
+document.getElementById("add-button")
+    .addEventListener("click",onClickAdd);
