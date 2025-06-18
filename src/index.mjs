6896af6a350eb9f6209d3ba91b626024ce01b51c
Libro <libro.1115@gmail.com>
2025-06-18 16:34:07 +0900

    完了・削除・戻るボタン

--- src/index.mjs
+++ src/index.mjs
@@ -12,11 +12,13 @@ const onClickAdd = ()=>{
     p.className="todo-item";
     p.innerText=inputText;
     //完了ボタン生成
-    const completeButton = document.createElement("button")
-    completeButton.innerText = "完了"
+    const completeButton = document.createElement("button");
+    completeButton.addEventListener("click",()=>{onClickComplete(completeButton);});
+    completeButton.innerText = "完了";
     //削除ボタン生成
-    const deleteButton = document.createElement("button")
-    deleteButton.innerText = "削除"
+    const deleteButton = document.createElement("button");
+    deleteButton.addEventListener("click",()=>{onClickDelete(deleteButton);});
+    deleteButton.innerText = "削除";
     //追加
     div.appendChild(p);
     div.appendChild(completeButton);
@@ -26,6 +28,33 @@ const onClickAdd = ()=>{
     console.log(li);
 }
 
-
+const onClickComplete = (completeButton) =>{
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    const backButton = document.createElement("button");
+    backButton.innerText ="戻す";
+    moveTarget.firstElementChild.appendChild(backButton);
+    document.getElementById("complete-list").appendChild(moveTarget);
+    backButton.addEventListener("click",()=>{onClickBack(backButton)});
+}
+const onClickDelete = (deleteButton) =>{
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+}
+const onClickBack = (backButton)=>
+{
+    const moveTarget = backButton.closest("li");
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了"
+    completeButton.addEventListener("click",()=>{onClickComplete(completeButton)});
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText="削除"
+    deleteButton.addEventListener("click",()=>onClickDelete(deleteButton));
+    backButton.remove();
+    moveTarget.firstElementChild.appendChild(completeButton);
+    moveTarget.firstElementChild.appendChild(deleteButton);
+    document.getElementById("incomplete-list").appendChild(moveTarget);
+}
 document.getElementById("add-button")
     .addEventListener("click",onClickAdd);
